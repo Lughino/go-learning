864b058323b990f7f68496a916691a96d8017d67
Luca Pau <luca@giffgaff.co.uk>
2019-08-24 13:29:09 +0100

    feat: add newDeck function

--- src/cards/deck.go
+++ src/cards/deck.go
@@ -3,6 +3,20 @@ package main
 // create a new type of desk chich is a slice of string
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		println(i, card)
--- src/cards/main.go
+++ src/cards/main.go
@@ -1,28 +1,7 @@
 package main
 
-import "fmt"
-
-var deckCount int
-
 func main() {
-	// var card = "Ace of Spades"
-	card := newCard()
-	deckCount = 5
-	fmt.Println("deck count", deckCount)
-	fmt.Println("simple card", card)
-
-	// cards := []string{"Five of Diamonds"}
-	cards := deck{"Five of Diamonds"}
-	cards = append(cards, "Six of Spades", newCard())
+	cards := newDeck()
 
 	cards.print()
-
-	// when index is not used, use a special "blank" character "_"
-	for _, card := range cards {
-		println("Without index", card)
-	}
-}
-
-func newCard() string {
-	return "Ace of Spades"
 }
