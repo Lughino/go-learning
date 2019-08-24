3c88bd0f5b559cbb331393d59c69b4074d8a93fe
Luca Pau <luca@giffgaff.co.uk>
2019-08-24 19:37:37 +0100

    feat: add saveToFile and readFromFile functions

--- src/cards/deck.go
+++ src/cards/deck.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
 // create a new type of desk chich is a slice of string
 type deck []string
 
@@ -25,4 +32,21 @@ func (d deck) print() {
 
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join(d, ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+	return deck(strings.Split(string(bs), ","))
 }
