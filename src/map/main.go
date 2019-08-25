4f7a126a0075ff5b09aa927497aa91d4b10bda7c
Luca Pau <luca@giffgaff.co.uk>
2019-08-25 12:09:19 +0100

    feat: add maps and difference between maps and struct

---
+++ src/map/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var colors map[string]string
+	// colors := make(map[string]string)
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#235666",
+	}
+
+	colors["white"] = "#ffffff"
+
+	// to delete an element inside the map
+	delete(colors, "green")
+
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println(color, hex)
+	}
+}
